6a8b927f152de745727f26542c6be8df4be82344
Arthur Tran <tran.art13@gmail.com>
2018-01-16 13:30:00 -0800

    ...

--- src/game/Aim.java
+++ src/game/Aim.java
@@ -13,15 +13,17 @@ public class Aim extends GameObject {
 	public static double vX, vY;
 	public static double deg = 10;
 	public static boolean up = true;
+	private double angle;
 	
 	public Aim(int x, int y, ID id) {
 		super(x, y, id);
-	}
-	public void tick() {
-		double angle = Math.toRadians(deg);
+
+		angle = Math.toRadians(deg);
 		
 		x2 = (x + 100 * Math.cos(angle));
 		y2 = (y + 100 * Math.sin(angle));
+	}
+	public void tick() {
 	}
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
