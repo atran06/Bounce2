cc4cb8b3f3da040ba6e2b5257b03f69ef66e5176
Arthur Tran <tran.art13@gmail.com>
2018-01-28 11:23:05 -0800

    Added header

--- src/game/Runner.java
+++ src/game/Runner.java
@@ -1,5 +1,10 @@
 package game;
 
+/*
+ * Created by Arthur Tran on 1/14/18
+ * Copyright © Arthur Tran. All rights reserved.
+ */
+
 public class Runner {
 
 	public static void main(String[] args) {
