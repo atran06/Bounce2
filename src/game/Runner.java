576eaa2db2865a1cda85e3c4246114743f675696
Arthur Tran <tran.art13@gmail.com>
2018-02-05 21:33:01 -0800

    Cleaned code

--- src/game/Runner.java
+++ src/game/Runner.java
@@ -2,7 +2,7 @@ package game;
 
 /*
  * Created by Arthur Tran on 1/14/18
- * Copyright © Arthur Tran. All rights reserved.
+ * Copyright ï¿½ Arthur Tran. All rights reserved.
  */
 
 public class Runner {
